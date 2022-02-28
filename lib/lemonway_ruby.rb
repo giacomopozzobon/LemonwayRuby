8f783893909fbce70f4e50d9cc477ef7e6ab17c0
nicolas.vandenbogaerde@gmail.com <nicolas.vandenbogaerde@gmail.com>
2022-02-28 10:01:55 +0100

    first commit

---
+++ lib/lemonway_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lemonway_ruby/version"
+
+module LemonwayRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lemonway_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LemonwayRuby
+  VERSION = "0.1.0"
+end
