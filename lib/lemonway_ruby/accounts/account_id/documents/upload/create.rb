e4664042ad2acc430e4fde670a46522ac9b97ece
nicolas.vandenbogaerde@gmail.com <nicolas.vandenbogaerde@gmail.com>
2022-03-01 13:00:48 +0100

    update gem

--- lib/lemonway_ruby/accounts/account_id/documents/upload/create.rb
+++ lib/lemonway_ruby/accounts/account_id/documents/upload/create.rb
@@ -6,6 +6,7 @@ module LemonwayRuby
           extend Helpers
 
           class << self
+
             def upload_file(account_id, hash = {})
               LemonwayRuby.authenticated do |access_token, token_type|
                 required_keys = %i[buffer type name]
@@ -17,7 +18,7 @@ module LemonwayRuby
                 #   }"
                 #   params = params.merge(other_params) unless other_params.empty?
                 #"#{url_conf}/accounts/#{accountid}/documents/upload"
-                conn_client.post(generate_uri.gsub!("accountid", account_id), params.to_s, "Content-Type" => "application/json") do |req|
+                conn_client.post(generate_uri.gsub!("accountid", account_id.to_s), params.to_json, "Content-Type" => "application/json") do |req|
                   req.headers['Authorization'] = "#{token_type} #{access_token}"
                   req.headers['PSU-IP-Address'] = LemonwayRuby.configuration.psu_ip_address
                 end
--- lib/lemonway_ruby/accounts/legal/create.rb
+++ lib/lemonway_ruby/accounts/legal/create.rb
@@ -6,10 +6,10 @@ module LemonwayRuby
       class << self
         def register_wallet(hash = {})
           LemonwayRuby.authenticated do |access_token, token_type|
-            required_keys = %i[company address accountId email firstName lastName nationality payerOrBeneficiary birth isDebtor]
+            required_keys = %i[company adresse accountId email firstName lastName nationality payerOrBeneficiary birth isDebtor]
             params = ensure_keys(hash, required_keys)
 
-            conn_client.post("#{generate_uri}", params.to_s, "Content-Type" => "application/json") do |req|
+            conn_client.post("#{generate_uri}", params.to_json, "Content-Type" => "application/json") do |req|
               req.headers['Authorization'] = "#{token_type} #{access_token}"
               req.headers['PSU-IP-Address'] = LemonwayRuby.configuration.psu_ip_address
             end
