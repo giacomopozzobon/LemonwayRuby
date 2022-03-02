0d83bf9d088bdb2031bdca462a798741e844905b
nicolas.vandenbogaerde@gmail.com <nicolas.vandenbogaerde@gmail.com>
2022-03-02 10:16:17 +0100

    remove comment code

--- lib/lemonway_ruby/accounts/account_id/documents/upload/create.rb
+++ lib/lemonway_ruby/accounts/account_id/documents/upload/create.rb
@@ -11,13 +11,6 @@ module LemonwayRuby
               LemonwayRuby.authenticated do |access_token, token_type|
                 required_keys = %i[buffer type name]
                 params = ensure_keys(hash, required_keys)
-                #   params = "{
-                #    'buffer': '#{buffer}',
-                #    'type': '#{type.to_i}',
-                #    'name': '#{name}'
-                #   }"
-                #   params = params.merge(other_params) unless other_params.empty?
-                #"#{url_conf}/accounts/#{accountid}/documents/upload"
                 conn_client.post(generate_uri.gsub!("accountid", account_id.to_s), params.to_json, "Content-Type" => "application/json") do |req|
                   req.headers['Authorization'] = "#{token_type} #{access_token}"
                   req.headers['PSU-IP-Address'] = LemonwayRuby.configuration.psu_ip_address
