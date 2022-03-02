187edcebf086cc94a4ead54278a57e8f23f36c11
nicolas.vandenbogaerde@gmail.com <nicolas.vandenbogaerde@gmail.com>
2022-03-02 16:45:49 +0100

    add get_wallet doc iban and account

---
+++ lib/lemonway_ruby/accounts/account_id/documents/index.rb
@@ -0,0 +1,20 @@
+module LemonwayRuby
+  module Accounts
+    module AccountId
+      module Documents
+        extend Helpers
+
+        class << self
+          def get_wallet_details_doc(account_id)
+            LemonwayRuby.authenticated do |access_token, token_type|
+              conn_client.get(generate_uri.gsub("accountid", account_id.to_s)) do |req|
+                req.headers['Authorization'] = "#{token_type} #{access_token}"
+                req.headers['PSU-IP-Address'] = LemonwayRuby.configuration.psu_ip_address
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/lemonway_ruby/moneyouts/account_id/iban/index.rb
@@ -0,0 +1,20 @@
+module LemonwayRuby
+  module Moneyouts
+    module AccountId
+      module Iban
+        extend Helpers
+
+        class << self
+          def get_wallet_details_iban(account_id)
+            LemonwayRuby.authenticated do |access_token, token_type|
+              conn_client.get(generate_uri.gsub("accountid", account_id.to_s)) do |req|
+                req.headers['Authorization'] = "#{token_type} #{access_token}"
+                req.headers['PSU-IP-Address'] = LemonwayRuby.configuration.psu_ip_address
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
