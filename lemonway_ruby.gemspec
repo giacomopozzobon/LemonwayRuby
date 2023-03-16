ffee4095588ad41199c446f7944f0d69efd89870
nicolas.vandenbogaerde@gmail.com <nicolas.vandenbogaerde@gmail.com>
2023-03-16 13:57:43 +0100

    update gem lemonway

--- lemonway_ruby.gemspec
+++ lemonway_ruby.gemspec
@@ -30,8 +30,8 @@ Gem::Specification.new do |spec|
   spec.bindir = "exe"
   spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
-  spec.add_dependency "activesupport"
-  spec.add_development_dependency "faraday_middleware"
+  spec.add_dependency "activesupport", "~> 6.1.5"
+  spec.add_development_dependency "faraday_middleware", "~> 1.2.0"
   spec.add_development_dependency "minitest"
   spec.add_development_dependency "minitest-reporters"
 
---
+++ lib/lemonway_ruby/moneyins/account_id/mandate/mandate_id/document/show.rb
@@ -0,0 +1,24 @@
+module LemonwayRuby
+  module Moneyins
+    module AccountId
+      module Mandate
+        module MandateId
+          module Document
+            extend Helpers
+          
+            class << self
+              def show(accountid, mandateid)
+                LemonwayRuby.authenticated do |access_token, token_type|
+                  conn_client.put(generate_uri.gsub("mandateid", mandateid.to_s).gsub("accountid", accountid.to_s), params.to_json, "Content-Type" => "application/json") do |req|
+                    req.headers['Authorization'] = "#{token_type} #{access_token}"
+                    req.headers['PSU-IP-Address'] = LemonwayRuby.configuration.psu_ip_address
+                  end
+                end
+              end
+            end 
+          end
+        end
+      end
+    end
+  end
+end
