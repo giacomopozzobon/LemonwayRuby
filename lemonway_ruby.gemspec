be257fea70f5054f78c777531cb62b7df81877ca
Guillaume BOUDON <guillaumeboudon@gmail.com>
2023-03-23 12:51:25 +0100

    :art: Clean code

--- lemonway_ruby.gemspec
+++ lemonway_ruby.gemspec
@@ -2,42 +2,23 @@
 
 require_relative "lib/lemonway_ruby/version"
 
-Gem::Specification.new do |spec|
-  spec.name = "lemonway_ruby"
-  spec.version = LemonwayRuby::VERSION
-  spec.authors = ["nicolas.vandenbogaerde@mipise.com"]
-  spec.email = ["nicolas.vandenbogaerde@mipise.com"]
+Gem::Specification.new do |s|
+  s.name = "lemonway_ruby"
+  s.version = "0.1.1"
+  s.summary = "Ruby SDK for Lemonway API"
+  s.description = "Ruby SDK for Lemonway API"
+  s.authors = ["nicolas.vandenbogaerde@mipise.com"]
+  s.email = ["nicolas.vandenbogaerde@mipise.com"]
+  s.homepage = "https://github.com/MIPISE/LemonwayRuby"
+  s.license = "MIT"
 
-  spec.summary = "Write a short summary, because RubyGems requires one."
-  spec.description = "Write a longer description or delete this line."
-  spec.homepage = "https://github.com/MIPISE/LemonwayRuby"
-  spec.license = "MIT"
-  spec.required_ruby_version = ">= 3.1.1"
+  s.required_ruby_version = ">= 3.1.1"
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to your gem server 'https://example.com'"
+  s.files = `git ls-files`.split("\n")
 
-  spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "https://github.com/MIPISE/LemonwayRuby"
-  spec.metadata["changelog_uri"] = "https://github.com/MIPISE/LemonwayRuby"
-
-  # Specify which files should be added to the gem when it is released.
-  # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
-  spec.files = Dir.chdir(File.expand_path(__dir__)) do
-    `git ls-files -z`.split("\x0").reject do |f|
-      (f == __FILE__) || f.match(%r{\A(?:(?:test|spec|features)/|\.(?:git|travis|circleci)|appveyor)})
-    end
-  end
-  spec.bindir = "exe"
-  spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
   spec.add_dependency "activesupport", "~> 6.1.5"
   spec.add_dependency "faraday_middleware", "~> 1.2.0"
+
   spec.add_development_dependency "minitest"
   spec.add_development_dependency "minitest-reporters"
-
-  # Uncomment to register a new dependency of your gem
-  # spec.add_dependency "example-gem", "~> 1.0"
-
-  # For more information and examples about making a new gem, checkout our
-  # guide at: https://bundler.io/guides/creating_gem.html
 end
--- lib/lemonway_ruby.rb
+++ lib/lemonway_ruby.rb
@@ -2,27 +2,24 @@
 
 require "active_support"
 require "active_support/core_ext/hash/indifferent_access"
-require_relative "lemonway_ruby/version"
-#require "faraday"
-#require 'ostruct'
-require_relative 'lemonway_ruby/configuration.rb'
-require_relative 'lemonway_ruby/base.rb'
-require_relative 'lemonway_ruby/helpers.rb'
-require_relative 'lemonway_ruby/client.rb'
-require_relative 'lemonway_ruby/accounts/legal/create.rb'
-require_relative 'lemonway_ruby/accounts/account_id/documents/upload/create.rb'
-require_relative 'lemonway_ruby/moneyouts/iban/create.rb'
-require_relative 'lemonway_ruby/moneyins/sdd/mandate/create.rb'
-require_relative 'lemonway_ruby/accounts/account_id/documents/document_id/signinit/create.rb'
-require_relative 'lemonway_ruby/moneyins/sdd/mandate/mandate_id/unregister/delete.rb'
-require_relative 'lemonway_ruby/accounts/kycstatus/account_id/update.rb'
-require_relative 'lemonway_ruby/moneyouts/account_id/iban/index.rb'
-require_relative 'lemonway_ruby/accounts/account_id/show.rb'
-require_relative 'lemonway_ruby/moneyins/account_id/mandate/mandate_id/document/show.rb'
-require_relative 'lemonway_ruby/accounts/account_id/documents/index.rb'
-require_relative 'lemonway_ruby/accounts/account_id/transactions/index.rb'
-require_relative 'lemonway_ruby/moneyins/bankwire/iban/create/create.rb'
-require_relative 'lemonway_ruby/moneyins/sdd/mandate/init/create.rb'
+require_relative "lemonway_ruby/configuration.rb"
+require_relative "lemonway_ruby/base.rb"
+require_relative "lemonway_ruby/helpers.rb"
+require_relative "lemonway_ruby/client.rb"
+require_relative "lemonway_ruby/accounts/legal/create.rb"
+require_relative "lemonway_ruby/accounts/account_id/documents/upload/create.rb"
+require_relative "lemonway_ruby/moneyouts/iban/create.rb"
+require_relative "lemonway_ruby/moneyins/sdd/mandate/create.rb"
+require_relative "lemonway_ruby/accounts/account_id/documents/document_id/signinit/create.rb"
+require_relative "lemonway_ruby/moneyins/sdd/mandate/mandate_id/unregister/delete.rb"
+require_relative "lemonway_ruby/accounts/kycstatus/account_id/update.rb"
+require_relative "lemonway_ruby/moneyouts/account_id/iban/index.rb"
+require_relative "lemonway_ruby/accounts/account_id/show.rb"
+require_relative "lemonway_ruby/moneyins/account_id/mandate/mandate_id/document/show.rb"
+require_relative "lemonway_ruby/accounts/account_id/documents/index.rb"
+require_relative "lemonway_ruby/accounts/account_id/transactions/index.rb"
+require_relative "lemonway_ruby/moneyins/bankwire/iban/create/create.rb"
+require_relative "lemonway_ruby/moneyins/sdd/mandate/init/create.rb"
 
 module LemonwayRuby
   class Error < Exception
@@ -32,13 +29,10 @@ module LemonwayRuby
       @code = code
     end
   end
-  # Your code goes here...
 
   class << self
-    #include Helper 
     include Configuration
     include Base
     include Client
-    #include Accounts
   end
 end
