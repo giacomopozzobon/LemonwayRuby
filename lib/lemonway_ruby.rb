b531ca38d992886633ae368719a64c053bcc90ad
Guillaume BOUDON <guillaumeboudon@gmail.com>
2023-03-29 15:45:10 +0200

    :recycle: Refactor code

--- lib/lemonway_ruby.rb
+++ lib/lemonway_ruby.rb
@@ -1,39 +1,16 @@
-# frozen_string_literal: true
-
 require "active_support"
 require "active_support/core_ext/hash/indifferent_access"
 
-require "lemonway_ruby/configuration"
-require "lemonway_ruby/base"
-require "lemonway_ruby/helpers"
-require "lemonway_ruby/client"
-require "lemonway_ruby/accounts/account_id/documents/document_id/signinit/create"
-require "lemonway_ruby/accounts/account_id/documents/index"
-require "lemonway_ruby/accounts/account_id/documents/upload/create"
-require "lemonway_ruby/accounts/account_id/show"
-require "lemonway_ruby/accounts/account_id/transactions/index"
-require "lemonway_ruby/accounts/kycstatus/account_id/update"
-require "lemonway_ruby/accounts/legal/create"
-require "lemonway_ruby/moneyins/account_id/mandate/mandate_id/document/show"
-require "lemonway_ruby/moneyins/bankwire/iban/create/create"
-require "lemonway_ruby/moneyins/sdd/mandate/create"
-require "lemonway_ruby/moneyins/sdd/mandate/init/create"
-require "lemonway_ruby/moneyins/sdd/mandate/mandate_id/unregister/delete"
-require "lemonway_ruby/moneyouts/account_id/iban/index"
-require "lemonway_ruby/moneyouts/iban/create"
+require_relative "lemonway_ruby/configuration"
+require_relative "lemonway_ruby/helpers"
+require_relative "lemonway_ruby/client"
+Dir["./lib/lemonway_ruby/accounts/**/*.rb"].each { |file| require file }
+Dir["./lib/lemonway_ruby/moneyins/**/*.rb"].each { |file| require file }
+Dir["./lib/lemonway_ruby/moneyouts/**/*.rb"].each { |file| require file }
 
 module LemonwayRuby
-  class Error < Exception
-    attr_reader :code
-
-    def initialize(code)
-      @code = code
-    end
-  end
-
   class << self
     include Configuration
-    include Base
     include Client
   end
 end
--- lib/lemonway_ruby/client.rb
+++ lib/lemonway_ruby/client.rb
@@ -1,3 +1,5 @@
+require "faraday_middleware"
+
 module LemonwayRuby
   module Client
     def conn_lemonway(request_content_type: :json)
@@ -20,6 +22,39 @@ module LemonwayRuby
 
     def url_conf
       "/mb#{LemonwayRuby.configuration.return_url}/#{LemonwayRuby.configuration.webservice}/#{LemonwayRuby.configuration.api_version}"
+    end
+
+    def conn(request_content_type: :json)
+      return @conn[request_content_type] if defined?(@conn) && @conn[request_content_type]
+      @conn ||= {}
+      @conn[request_content_type] = Faraday.new(
+        url: "https://#{LemonwayRuby.configuration.url_auth}",
+        headers: {
+          "accept" => "application/json;charset=UTF-8",
+          "Content-Type" => "application/x-www-form-urlencoded"
+        },
+        proxy: LemonwayRuby.configuration.proxy
+      ) do |conn|
+        conn.request request_content_type
+        conn.response :json, content_type: /\bjson$/
+        conn.adapter Faraday.default_adapter
+      end
+      @conn[request_content_type]
+    end
+
+    def auth
+      conn(request_content_type: :url_encoded).post("/oauth/api/v1/oauth/token", {Grant_type: LemonwayRuby.configuration.grant_type}) do |req|
+        req.headers["Authorization"] = LemonwayRuby.configuration.authorization
+      end
+    end
+
+    def authenticated
+      response = auth
+      if response.success?
+        access_token, token_type = response.body.values_at("access_token", "token_Type")
+        response = yield(access_token, token_type)
+      end
+      response
     end
   end
 end
