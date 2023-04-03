7fadd186d0af4c82fb1d02638555977eb0709fa8
Guillaume BOUDON <guillaumeboudon@gmail.com>
2023-04-03 15:03:54 +0200

    :bug: Fix a bug

--- lib/lemonway_ruby.rb
+++ lib/lemonway_ruby.rb
@@ -1,11 +1,22 @@
 require "active_support/core_ext/hash/indifferent_access"
-
 require_relative "lemonway_ruby/configuration"
 require_relative "lemonway_ruby/helpers"
 require_relative "lemonway_ruby/client"
-Dir["./lib/lemonway_ruby/accounts/**/*.rb"].each { |file| require file }
-Dir["./lib/lemonway_ruby/moneyins/**/*.rb"].each { |file| require file }
-Dir["./lib/lemonway_ruby/moneyouts/**/*.rb"].each { |file| require file }
+
+require_relative "lemonway_ruby/accounts/account_id/documents/document_id/signinit/create"
+require_relative "lemonway_ruby/accounts/account_id/documents/index"
+require_relative "lemonway_ruby/accounts/account_id/documents/upload/create"
+require_relative "lemonway_ruby/accounts/account_id/show"
+require_relative "lemonway_ruby/accounts/account_id/transactions/index"
+require_relative "lemonway_ruby/accounts/kycstatus/account_id/update"
+require_relative "lemonway_ruby/accounts/legal/create"
+require_relative "lemonway_ruby/moneyins/account_id/mandate/mandate_id/document/show"
+require_relative "lemonway_ruby/moneyins/bankwire/iban/create/create"
+require_relative "lemonway_ruby/moneyins/sdd/mandate/create"
+require_relative "lemonway_ruby/moneyins/sdd/mandate/init/create"
+require_relative "lemonway_ruby/moneyins/sdd/mandate/mandate_id/unregister/delete"
+require_relative "lemonway_ruby/moneyouts/account_id/iban/index"
+require_relative "lemonway_ruby/moneyouts/iban/create"
 
 module LemonwayRuby
   class << self
