563b2a31c79dc7b507149a89a5e8bfe95ad334e7
giacomopozzobon <g97.pozzobon@gmail.com>
2024-12-20 10:57:03 +0100

    Optimize Loading Required Files

--- lib/lemonway_ruby.rb
+++ lib/lemonway_ruby.rb
@@ -1,25 +1,33 @@
-require "active_support/core_ext/hash/indifferent_access"
-require_relative "lemonway_ruby/configuration"
-require_relative "lemonway_ruby/helpers"
-require_relative "lemonway_ruby/client"
+# *******************************
+# lemonway_ruby - Loading modules and configurations for the LemonwayRuby gem.
+#
+# This file is responsible for loading all the necessary Ruby modules for the correct functioning of the
+# `LemonwayRuby` gem. It includes the configuration and client to interact with the Lemonway services.
+#
+# 1. The `active_support/core_ext/hash/indifferent_access` library is imported to allow hash access with 
+#    both symbols and strings interchangeably.
+#
+# 2. The `helpers.rb` file is loaded, containing helper methods used by other parts of the gem.
+#
+# 3. The following section dynamically loads all Ruby modules and classes from the `lemonway_ruby` directory,
+#    including subdirectories, to avoid having to manually write a `require` statement for each file.
+#
+# 4. The `LemonwayRuby` module is configured, including the necessary configuration and client modules to interact 
+#    with the Lemonway API.
+#
+# Make sure to update this section with additional details in case of significant changes to the flow or structure 
+# of the project.
+# *******************************
 
-require_relative "lemonway_ruby/accounts/account_id/documents/document_id/signinit/create"
-require_relative "lemonway_ruby/accounts/account_id/documents/index"
-require_relative "lemonway_ruby/accounts/account_id/documents/upload/create"
-require_relative "lemonway_ruby/accounts/account_id/show"
-require_relative "lemonway_ruby/accounts/account_id/transactions/index"
-require_relative "lemonway_ruby/accounts/individual/create"
-require_relative "lemonway_ruby/accounts/kycstatus/account_id/update"
-require_relative "lemonway_ruby/accounts/legal/create"
-require_relative "lemonway_ruby/moneyins/account_id/mandate/mandate_id/document/show"
-require_relative "lemonway_ruby/moneyins/bankwire/iban/create/create"
-require_relative "lemonway_ruby/moneyins/card/webinit/create"
-require_relative "lemonway_ruby/moneyins/index"
-require_relative "lemonway_ruby/moneyins/sdd/mandate/create"
-require_relative "lemonway_ruby/moneyins/sdd/mandate/init/create"
-require_relative "lemonway_ruby/moneyins/sdd/mandate/mandate_id/unregister/delete"
-require_relative "lemonway_ruby/moneyouts/account_id/iban/index"
-require_relative "lemonway_ruby/moneyouts/iban/create"
+
+
+require "active_support/core_ext/hash/indifferent_access"
+require_relative 'lemonway_ruby/helpers'
+
+
+Dir[File.join(__dir__, 'lemonway_ruby', '**', '*.rb')].sort.each { |file| require file }
+
+
 
 module LemonwayRuby
   class << self
